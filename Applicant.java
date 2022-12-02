567ec0ef0d095409ce5e197cd4b7218936962884
Jasmyn622 <shimzheng@student.unimelb.edu.au>
2022-12-02 19:47:24 +1100

    Create Applicant.java

---
+++ Applicant.java
@@ -0,0 +1,300 @@
+/**
+ * COMP90041, Sem2, 2022: Final Project
+ * @author
+ * Name: Shiming ZHENG
+ * Email: shimzheng@student.unimelb.edu.au
+ * Student number: 1149897
+ */
+
+/**
+ * The class for current applicant
+ */
+public class Applicant {
+
+    /**
+     * applicant's lastname
+     */
+    private String lastname;
+
+    /**
+     * applicant's firstname
+     */
+    private String firstname;
+
+    /**
+     * applicant's career summary
+     */
+    private String careerSummary;
+
+    /**
+     * applicant's age
+     */
+    private int age;
+
+    /**
+     * applicant's gender
+     */
+    private String gender;
+
+    /**
+     * applicant's highest degree
+     */
+    private String highestDegree;
+
+    /**
+     * applicant's grade of coursework90041
+     */
+    private int coursework90041;
+
+    /**
+     * applicant's grade of coursework90038
+     */
+    private int coursework90038;
+
+    /**
+     * applicant's grade of coursework90007
+     */
+    private int coursework90007;
+
+    /**
+     * applicant's grade of coursework90002
+     */
+    private int coursework90002;
+
+    /**
+     * applicant's salary expectation
+     */
+    private int salaryExpectation;
+
+    /**
+     * applicant's availability
+     */
+    private String availabilityDate;
+
+    /**
+     * the default constructor of the Applicant class
+     */
+    public Applicant() {
+    }
+
+    // Getter
+
+    /**
+     * get the applicant's lastname
+     *
+     * @return the applicant's lastname
+     */
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    /**
+     * get the applicant's firstname
+     *
+     * @return the applicant's firstname
+     */
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    /**
+     * get the applicant's career summary
+     *
+     * @return the applicant's career summary
+     */
+    public String getCareerSummary() {
+        return careerSummary;
+    }
+
+    /**
+     * get the applicant's age
+     *
+     * @return the applicant's age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * get the applicant's gender
+     *
+     * @return the applicant's gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * get the applicant's highest degree
+     *
+     * @return the applicant's highest degree
+     */
+    public String getHighestDegree() {
+        return highestDegree;
+    }
+
+    /**
+     * get the applicant's grade of coursework90041
+     *
+     * @return the applicant's grade of coursework90041
+     */
+    public int getCoursework90041() {
+        return coursework90041;
+    }
+
+    /**
+     * get the applicant's grade of coursework90038
+     *
+     * @return the applicant's grade of coursework90038
+     */
+    public int getCoursework90038() {
+        return coursework90038;
+    }
+
+    /**
+     * get the applicant's grade of coursework90007
+     *
+     * @return the applicant's grade of coursework90007
+     */
+    public int getCoursework90007() {
+        return coursework90007;
+    }
+
+    /**
+     * get the applicant's grade of coursework90002
+     *
+     * @return the applicant's grade of coursework90002
+     */
+    public int getCoursework90002() {
+        return coursework90002;
+    }
+
+    /**
+     * get the applicant's salary expectation
+     *
+     * @return the applicant's salary expectation
+     */
+    public int getSalaryExpectation() {
+        return salaryExpectation;
+    }
+
+    /**
+     * get the applicant's availability
+     *
+     * @return the applicant's availability
+     */
+    public String getAvailabilityDate() {
+        return availabilityDate;
+    }
+
+    // Setter
+
+    /**
+     * set the applicant's lastname
+     *
+     * @param lastname applicant's lastname
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * set the applicant's firstname
+     *
+     * @param firstname applicant's firstname
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * set the applicant's career summary
+     *
+     * @param careerSummary applicant's career summary
+     */
+    public void setCareerSummary(String careerSummary) {
+        this.careerSummary = careerSummary;
+    }
+
+    /**
+     * set the applicant's age
+     *
+     * @param age applicant's age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * set the applicant's gender
+     *
+     * @param gender applicant's gender
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * set the applicant's highest degree
+     *
+     * @param highestDegree applicant's highest degree
+     */
+    public void setHighestDegree(String highestDegree) {
+        this.highestDegree = highestDegree;
+    }
+
+    /**
+     * set the applicant's grade of coursework90041
+     *
+     * @param coursework90041 applicant's grade of coursework90041
+     */
+    public void setCoursework90041(int coursework90041) {
+        this.coursework90041 = coursework90041;
+    }
+
+    /**
+     * set the applicant's grade of coursework90038
+     *
+     * @param coursework90038 applicant's grade of coursework90038
+     */
+    public void setCoursework90038(int coursework90038) {
+        this.coursework90038 = coursework90038;
+    }
+
+    /**
+     * set the applicant's grade of coursework90007
+     *
+     * @param coursework90007 applicant's grade of coursework90007
+     */
+    public void setCoursework90007(int coursework90007) {
+        this.coursework90007 = coursework90007;
+    }
+
+    /**
+     * set the applicant's grade of coursework90002
+     *
+     * @param coursework90002 applicant's grade of coursework90002
+     */
+    public void setCoursework90002(int coursework90002) {
+        this.coursework90002 = coursework90002;
+    }
+
+    /**
+     * set the applicant's salary expectation
+     *
+     * @param salaryExpectation applicant's salary expectation
+     */
+    public void setSalaryExpectation(int salaryExpectation) {
+        this.salaryExpectation = salaryExpectation;
+    }
+
+    /**
+     * set the applicant's availability
+     *
+     * @param availabilityDate applicant's availability
+     */
+    public void setAvailabilityDate(String availabilityDate) {
+        this.availabilityDate = availabilityDate;
+    }
+
+}
